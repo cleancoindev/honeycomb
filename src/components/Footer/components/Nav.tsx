2d5f938b83186d586e0e59c6d8ac27a3be94b78d
Monstrosity1 <richburmei@hotmail.com>
2020-11-24 19:59:16 -0500

    added link to honeyswap

--- src/components/Footer/components/Nav.tsx
+++ src/components/Footer/components/Nav.tsx
@@ -8,17 +8,20 @@ const Nav: React.FC = () => {
       <StyledHeader>
         Community
       </StyledHeader>
-      <Link href="https://discord.gg/qPa4h5w" external>
-        Discord
-      </Link>
       <Link href="https://github.com/1Hive" external>
         Github
+      </Link>
+      <Link href="https://discord.gg/wcNg589r" external>
+        Help
+      </Link>
+      <Link href="https://forum.1hive.org/" external>
+        Forum
       </Link>
       <Link href="https://twitter.com/1HiveOrg" external>
         Twitter
       </Link>
-      <Link href="https://forum.1hive.org/" external>
-        Forum
+      <Link href="https://t.me/honeyswapdex" external>
+        Telegram
       </Link>
     </div>
   )
